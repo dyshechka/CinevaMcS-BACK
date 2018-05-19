33ab704c9243956f9670252c28f52ec75b95a7f0
garro <ezenhorn@mail.ru>
2018-05-19 22:45:41 +0300

    finalize getting film process

--- film-service/src/main/java/cinema/cloud/service/film/service/FilmService.java
+++ film-service/src/main/java/cinema/cloud/service/film/service/FilmService.java
@@ -46,21 +46,43 @@ public class FilmService {
 
     @Transactional
     public ArrayList<FilmWithSeance> getFilmsWithSeances(Long dateTime) {
-        Calendar calendar = Calendar.getInstance();
-        DateTime limit = new DateTime(dateTime);
-        calendar.set(limit.getYear(), limit.getMonthOfYear() - 1, limit.getDayOfMonth() + 1, 23, 59);
+        DateTime incDate = new DateTime(dateTime);
+        DateTime today = DateTime.now();
         ArrayList<FilmWithSeance> filmsWithSeances = new ArrayList<>();
-        ArrayList<Film> filmsByDate = getFilmsByDate(dateTime);
-        filmsByDate.forEach(film -> {
-            ArrayList<Seance> seancesForFilm = getSeancesForFilm(film.getId(), dateTime);
-            ArrayList<Seance> seances = seancesForFilm.stream().filter(seance ->
-                    seance.getTime().after(new Date(dateTime)))
-                    .filter(seance -> seance.getTime().before(calendar.getTime()))
-                    .collect(Collectors.toCollection(ArrayList::new));
-            if (!seances.isEmpty()) {
-                filmsWithSeances.add(new FilmWithSeance(film, seances));
-            }
-        });
+        if (incDate.getDayOfMonth() == today.getDayOfMonth() && incDate.getMonthOfYear() == today.getMonthOfYear()) {
+            // Today
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(today.getYear(), today.getMonthOfYear() - 1, today.getDayOfMonth(), 23, 59);
+            ArrayList<Film> filmsByDate = getFilmsByDate(dateTime);
+            filmsByDate.forEach(film -> {
+                ArrayList<Seance> seancesForFilm = getSeancesForFilm(film.getId(), dateTime);
+                ArrayList<Seance> seances = seancesForFilm.stream().filter(seance ->
+                        seance.getTime().after(new Date(dateTime)))
+                        .filter(seance -> seance.getTime().before(calendar.getTime()))
+                        .collect(Collectors.toCollection(ArrayList::new));
+                if (!seances.isEmpty()) {
+                    filmsWithSeances.add(new FilmWithSeance(film, seances));
+                }
+            });
+        } else {
+            Calendar calendar = Calendar.getInstance();
+            DateTime targetTime = new DateTime(dateTime);
+            calendar.set(targetTime.getYear(), targetTime.getMonthOfYear() - 1, targetTime.getDayOfMonth(), 23, 59);
+            Date limit = calendar.getTime();
+            calendar.set(targetTime.getYear(), targetTime.getMonthOfYear() - 1, targetTime.getDayOfMonth(), 0, 0);
+            Date start = calendar.getTime();
+            ArrayList<Film> filmsByDate = getFilmsByDate(dateTime);
+            filmsByDate.forEach(film -> {
+                ArrayList<Seance> seancesForFilm = getSeancesForFilm(film.getId(), dateTime);
+                ArrayList<Seance> seances = seancesForFilm.stream().filter(seance ->
+                        seance.getTime().after(start))
+                        .filter(seance -> seance.getTime().before(limit))
+                        .collect(Collectors.toCollection(ArrayList::new));
+                if (!seances.isEmpty()) {
+                    filmsWithSeances.add(new FilmWithSeance(film, seances));
+                }
+            });
+        }
 
         return filmsWithSeances;
     }
